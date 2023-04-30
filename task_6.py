72f1d129dcd8a119192afba1b81ef41f987b9eff
Iliapoleshuk <127932109+Iliapoleshuk@users.noreply.github.com>
2023-04-30 13:15:50 +0200

    Create task_6.py

---
+++ task_6.py
@@ -0,0 +1,20 @@
+for b in range (1, 10):
+        a = 1
+        c = 2
+        d = 3
+        e = 4
+        g = 5
+        r = 6
+        y = 7
+        u = 8
+        i = 9
+        print(a * b, end='\t')
+        print(c * b, end='\t')
+        print(d * b, end='\t')
+        print(e * b, end='\t')
+        print(g * b, end='\t')
+        print(r * b, end='\t')
+        print(y * b, end='\t')
+        print(u * b, end='\t')
+        print(i * b, end='\t')
+        print(' ')
