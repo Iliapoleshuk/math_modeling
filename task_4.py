89f7a47c8492b27d4ca80b1f86562ce7eb014fd1
Iliapoleshuk <127932109+Iliapoleshuk@users.noreply.github.com>
2023-05-07 22:46:06 +0200

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -2,8 +2,8 @@ a = int(input())
 m = 1
 n = 2
 l = 0
-print(end=" ", l , m , n)
-for i in range(4,a+1):
+print(l , m, m, n, end=" ")
+for i in range(5,a+1):
   print(m + n , end=" ")
   c = m
   m = n
