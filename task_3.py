cd46dcefdec0d60ba61c0dfee886497f8cac2ef7
Iliapoleshuk <127932109+Iliapoleshuk@users.noreply.github.com>
2023-05-08 20:49:03 +0200

    task_3.py

---
+++ task_3.py
@@ -0,0 +1,13 @@
+import numpy as np
+y = int(input())
+x = int(input())
+v = int(input())
+g = 10
+c []
+k = np.array(c)
+for t in range(0,6,1):
+    s = x + v*t
+    l = y + v*t - (g*t**2)/2
+    a = [t,s,l]
+    b = np.array(a)
+    print(b)
