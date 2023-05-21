89eebdc972cb9a5c22639002e40608ab58cdfee8
Iliapoleshuk <127932109+Iliapoleshuk@users.noreply.github.com>
2023-05-21 18:44:38 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -3,11 +3,10 @@ y = int(input())
 x = int(input())
 v = int(input())
 g = 10
-c []
-k = np.array(c)
-for t in range(0,6,1):
+t= np.arange(0,5.01,0.01)
+for i in range(0,len(t),1):
     s = x + v*t
     l = y + v*t - (g*t**2)/2
     a = [t,s,l]
-    b = np.array(a)
-    print(b)
+k = np.array(a)
+print(k)
